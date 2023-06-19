f678c4f12c991163f7c2c513626e6f19e679a298
Zimolev <zimolev@gmail.com>
2023-06-19 16:08:04 +0400

    Отформатировал

--- src/main/java/org/example/HomeWork/HomeWorkFive.java
+++ src/main/java/org/example/HomeWork/HomeWorkFive.java
@@ -1,7 +1,5 @@
 package org.example.HomeWork;
 
-import java.util.Arrays;
-
 /**
  * при входном массиве, вернуть массив массивов со всеми перестановками его элементов.
  * Пример входных данных:
@@ -10,7 +8,8 @@ import java.util.Arrays;
  * [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  */
 public class HomeWorkFive {
-    int[] array = {1,2,3};
+    int[] array = {1, 2, 3};
+
     void swap(int[] arr, int x, int y) {
         int temp = arr[x];
         arr[x] = arr[y];
